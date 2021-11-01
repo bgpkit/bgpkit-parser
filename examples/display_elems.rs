16db6f585bb305279268d4861d50cd7dde9dc408
Mingwei Zhang <mingwei@mwzhang.com>
2021-11-01 14:00:52 -0700

    add example for displaying parsed elems

---
+++ examples/display_elems.rs
@@ -0,0 +1,16 @@
+use bgpkit_parser::BgpkitParser;
+
+fn main() {
+    let url =
+        "http://archive.routeviews.org/bgpdata/\
+        2021.10/UPDATES/updates.20211001.0000.bz2";
+    for elem in BgpkitParser::new(url) {
+        println!("{:?}|{:?}|{:?}|{:?}|{:?}",
+            elem.elem_type,
+            elem.timestamp,
+            elem.prefix,
+            elem.as_path,
+            elem.next_hop,
+        )
+    }
+}
