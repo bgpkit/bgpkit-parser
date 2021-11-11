f66287c4db8609ffa5fe41329e28a1fc00ac8e70
Mingwei Zhang <mingwei@mwzhang.com>
2021-11-11 15:50:06 -0800

    revamp kafka example
    it now uses [`kafka-rust`](https://github.com/kafka-rust/kafka-rust) and
    the example is much more concise than the previous version.  the kafka
    stream can stay alive much longer in this version.
    
    we also added BgpMessage to BgpElems conversion, so that we can print
    out messages with more standard single-line BGP messages format.

--- examples/routeviews-kafka.rs
+++ examples/routeviews-kafka.rs
@@ -1,73 +1,75 @@
 use std::io::Cursor;
-use rdkafka::{ClientConfig, ClientContext, Message};
-use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, StreamConsumer};
+use std::thread::sleep;
+use std::time::Duration;
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+use kafka::error::Error as KafkaError;
 pub use bgpkit_parser::{parse_bmp_msg, parse_openbmp_header};
 use log::{info, error};
+use bgpkit_parser::Elementor;
 use bgpkit_parser::parser::bmp::messages::MessageBody;
 
-// A simple context to customize the consumer behavior and print a log line every time
-// offsets are committed
-struct LoggingConsumerContext;
+fn consume_and_print(group: String, topic: String, brokers: Vec<String>) -> Result<(), KafkaError>{
 
-impl ClientContext for LoggingConsumerContext {}
-impl ConsumerContext for LoggingConsumerContext {}
-
-// Define a new type for convenience
-type LoggingConsumer = StreamConsumer<LoggingConsumerContext>;
-
-async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
-    let consumer: LoggingConsumer = ClientConfig::new()
-        .set("group.id", group_id)
-        .set("bootstrap.servers", brokers)
-        .set("enable.partition.eof", "false")
-        .set("session.timeout.ms", "60000")
-        .set("enable.auto.commit", "true")
-        //.set("statistics.interval.ms", "30000")
-        .set("auto.offset.reset", "latest")
-        .create_with_context(LoggingConsumerContext).unwrap();
-
-    consumer
-        .subscribe(&topics.to_vec())
-        .expect("Can't subscribe to specified topics");
+    let mut con = Consumer::from_hosts(brokers)
+        .with_topic(topic)
+        .with_group(group)
+        .with_fetch_max_bytes_per_partition(100_000)
+        .with_retry_max_bytes_limit(1_000_000)
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_offset_storage(GroupOffsetStorage::Kafka)
+        .create()?;
 
     loop {
-        match consumer.recv().await {
-            Err(_) => {},
-            Ok(m) => {
-                let payload = m.payload();
-                if let Some(p) = payload {
-                    let mut reader = Cursor::new(Vec::from(p));
-                    let header = parse_openbmp_header(&mut reader).unwrap();
-                    match parse_bmp_msg(&mut reader) {
-                        Ok(msg) => {
-                            info!("Parsing OK: {:?}", msg.common_header.msg_type);
-                            match msg.message_body {
-                                MessageBody::RouteMonitoring(m) => {
-                                    dbg!(m.bgp_update);
+        let mss = con.poll()?;
+        if mss.is_empty() {
+            println!("No messages available right now, wait for 5 seconds.");
+            sleep(Duration::from_secs(5));
+            continue
+        }
+
+        for ms in mss.iter() {
+            for m in ms.messages() {
+                let bytes = m.value;
+                let mut reader = Cursor::new(Vec::from(bytes));
+                let header = parse_openbmp_header(&mut reader).unwrap();
+                match parse_bmp_msg(&mut reader) {
+                    Ok(msg) => {
+                        let timestamp = header.timestamp;
+                        let per_peer_header = msg.per_peer_header.unwrap();
+                        match msg.message_body {
+                            MessageBody::RouteMonitoring(m) => {
+                                for elem in Elementor::bgp_to_elems(
+                                    m.bgp_message,
+                                    timestamp,
+                                    &per_peer_header.peer_ip,
+                                    &per_peer_header.peer_asn
+                                )
+                                {
+                                    info!("{}", elem);
                                 }
-                                _ => {}
                             }
-                        }
-                        Err(e) => {
-                            error!("{:?}", e);
-                            error!("{:?}", header);
-                            let hex = hex::encode(p);
-                            error!("{}", hex);
-                            break
+                            _ => {}
                         }
                     }
-
+                    Err(_e) => {
+                        let hex = hex::encode(bytes);
+                        error!("{}", hex);
+                        break
+                    }
                 }
-                consumer.commit_message(&m, CommitMode::Async).unwrap();
             }
-        };
+            let _ = con.consume_messageset(ms);
+        }
+        con.commit_consumed()?;
     }
 }
 
-#[tokio::main]
-pub async fn main(){
-    env_logger::init();
-    let topic = "^routeviews\\.route-views2\\..+\\.bmp_raw";
-    // consume_and_print("stream.routeviews.org", "bgpkit-parser-2", &["routeviews.route-views2.7660.bmp_raw"]).await
-    consume_and_print("stream.routeviews.org", "bgpkit-parser-2", &[topic]).await
+pub fn main(){
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let broker = "stream.routeviews.org:9092".to_owned();
+    let topic = "routeviews.linx.6830.bmp_raw".to_owned();
+    let group = "bgpkit-parser-example".to_owned();
+
+    consume_and_print(group, topic, vec![broker]).unwrap();
 }
