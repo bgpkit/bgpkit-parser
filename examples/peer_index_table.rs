90086cc4eb483576ab5aa80fb21b98cdb603587a
Mingwei Zhang <mingwei@mwzhang.com>
2021-11-28 12:30:25 -0800

    add peer-index-table example

---
+++ examples/peer_index_table.rs
@@ -0,0 +1,14 @@
+use bgp_models::mrt::MrtRecord;
+use serde_json::{json, to_string_pretty};
+
+/// This example reads from TableDumpV2-formatted RIB dump from RIPE RIS and print out the JSON-formatted
+/// peer index table.
+fn main() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+    // a 1.3GB rib dump file from rrc03
+    let url = "https://data.ris.ripe.net/rrc03/2021.11/bview.20211128.1600.gz";
+    let parser = bgpkit_parser::BgpkitParser::new(url).unwrap();
+    for record in parser.into_record_iter().take(1) {
+        println!("{}", to_string_pretty(&json!(record)).unwrap());
+    }
+}
