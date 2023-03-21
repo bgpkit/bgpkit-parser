8de7532ec3555d83bb2fbed9c49e68071cfac2c8
Mingwei Zhang <mingwei@mwzhang.com>
2023-03-21 09:47:51 -0700

    clean up outputs

--- bgpkit-parser/examples/find_as_set_messages.rs
+++ bgpkit-parser/examples/find_as_set_messages.rs
@@ -1,7 +1,6 @@
 use bgp_models::bgp::AsPathSegment;
 use bgpkit_broker::BgpkitBroker;
 use bgpkit_parser::BgpkitParser;
-use itertools::Itertools;
 use rayon::prelude::*;
 use std::collections::HashSet;
 
@@ -15,35 +14,31 @@ fn main() {
         .unwrap();
 
     println!("start parsing {} ribs in parallel", items.len());
-    let res: Vec<(String, HashSet<u32>)> = items
-        .par_iter()
-        .flat_map(|item| {
-            let parser = BgpkitParser::new(item.url.as_str()).unwrap();
-            let collector = item.collector_id.clone();
-            let mut origins: HashSet<u32> = HashSet::new();
-            for elem in parser {
-                if !elem.elem_type.is_announce() {
-                    continue;
-                }
-                for seg in elem.as_path.as_ref().unwrap().segments() {
-                    match seg {
-                        AsPathSegment::AsSet(p) | AsPathSegment::ConfedSet(p) => {
-                            println!("{elem}");
-                            origins.insert(p.last().unwrap().asn);
-                        }
-                        _ => {}
+    items.par_iter().for_each(|item| {
+        let parser = BgpkitParser::new(item.url.as_str()).unwrap();
+        let collector = item.collector_id.clone();
+        let mut origins: HashSet<u32> = HashSet::new();
+        for elem in parser {
+            if !elem.elem_type.is_announce() {
+                continue;
+            }
+            for seg in elem.as_path.as_ref().unwrap().segments() {
+                match seg {
+                    AsPathSegment::AsSet(p) | AsPathSegment::ConfedSet(p) => {
+                        // println!("{elem}");
+                        origins.insert(p.last().unwrap().asn);
                     }
+                    _ => {}
                 }
             }
+        }
 
-            return Some((collector, origins));
-        })
-        .collect();
-
-    for (collector, origins) in res {
-        println!(
-            "{collector}\n{}",
-            origins.iter().map(|v| v.to_string()).join(", ")
-        );
-    }
+        if !origins.is_empty() {
+            println!(
+                "found {} origins with AS set from {} collector",
+                origins.len(),
+                collector.as_str()
+            );
+        }
+    });
 }
