4bc4e44ad619efa571be50c8c93d76c3e566b1f7
Mingwei Zhang <mingwei@mwzhang.com>
2023-04-08 22:55:07 -0700

    revise example documentation and comments

--- bgpkit-parser/examples/cache_reading.rs
+++ bgpkit-parser/examples/cache_reading.rs
@@ -1,8 +1,7 @@
 use bgpkit_broker::BgpkitBroker;
 use bgpkit_parser::{BgpElem, BgpkitParser};
 
-/// This example shows how use BGPKIT Broker to retrieve a number of data file pointers that matches
-/// the time range criteria, and then parse the data files for each one.
+/// This example shows how use parser's cache feature to read data files from the local disk.
 fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
@@ -15,8 +14,6 @@ fn main() {
     for item in broker.query().unwrap() {
         let parser =
             BgpkitParser::new_cached(item.url.as_str(), "/tmp/bgpkit-cache-example/").unwrap();
-        // dbg!(&item);
-        // println!("{}", item.url);
         // iterating through the parser. the iterator returns `BgpElem` one at a time.
         let elems = parser.into_elem_iter().collect::<Vec<BgpElem>>();
         log::info!("{} {} {}", item.collector_id, item.url, elems.len());
