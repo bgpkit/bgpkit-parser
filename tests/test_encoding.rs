b27662f196a804eaa54f368ed43df6270cd94894
Mingwei Zhang <mingwei@mwzhang.com>
2023-08-12 11:33:53 -0700

    better test for v1 tabledump

--- tests/test_encoding.rs
+++ tests/test_encoding.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests {
+    use bgpkit_parser::models::MrtRecord;
     use bgpkit_parser::{parse_mrt_record, BgpkitParser};
-    use std::io::Cursor;
+    use std::io::{Cursor, Write};
 
     #[test]
     fn test_encode_small() {
@@ -28,11 +29,31 @@ mod tests {
     #[test]
     fn test_encode_table_dump_v1() {
         let url = "http://archive.routeviews.org/bgpdata/2002.01/RIBS/rib.20020101.0027.bz2";
-        let parser = BgpkitParser::new(url).unwrap();
-        for record in parser.into_record_iter() {
+        let input_records: Vec<MrtRecord> =
+            BgpkitParser::new(url).unwrap().into_record_iter().collect();
+
+        let dir = tempdir::TempDir::new("test_encode_table_dump_v1").unwrap();
+        let tempfile = dir
+            .path()
+            .join("test_encode_table_dump_v1.mrt")
+            .to_str()
+            .unwrap()
+            .to_string();
+        let mut writer = oneio::get_writer(tempfile.as_str()).unwrap();
+
+        for record in input_records.iter() {
             let bytes = record.encode();
+            writer.write_all(&bytes).unwrap();
             let parsed_record = parse_mrt_record(&mut Cursor::new(bytes)).unwrap();
-            assert_eq!(record, parsed_record);
+            assert_eq!(*record, parsed_record);
         }
+        drop(writer);
+
+        let encoded_records: Vec<MrtRecord> = BgpkitParser::new(tempfile.as_str())
+            .unwrap()
+            .into_record_iter()
+            .collect();
+
+        assert_eq!(input_records, encoded_records);
     }
 }
