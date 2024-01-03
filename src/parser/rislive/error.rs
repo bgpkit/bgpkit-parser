1bc0f4eea7a632ab44b744eda2b771e673fce676
Mingwei Zhang <mingwei@mwzhang.com>
2024-01-03 11:49:40 -0800

    add rislive tests

--- src/parser/rislive/error.rs
+++ src/parser/rislive/error.rs
@@ -56,3 +56,78 @@ impl convert::From<serde_json::Error> for ParserRisliveError {
 }
 
 impl Error for ParserRisliveError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::Deserialize;
+
+    #[test]
+    fn test_convert_from_serde_json_error() {
+        #[derive(Deserialize)]
+        struct TestError;
+
+        let json: Result<TestError, _> = serde_json::from_str("test");
+        let err = ParserRisliveError::from(json.err().unwrap());
+        assert_eq!(err.to_string(), "incorrect json message: serde_json error");
+    }
+
+    #[test]
+    fn test_ris_live_error_display() {
+        let err = ParserRisliveError::IncorrectJson("test".to_string());
+        assert_eq!(err.to_string(), "incorrect json message: test");
+
+        let err = ParserRisliveError::IncorrectRawBytes;
+        assert_eq!(err.to_string(), "incorrect raw bytes");
+
+        let err = ParserRisliveError::UnsupportedMessage;
+        assert_eq!(err.to_string(), "unsupported message");
+
+        let err = ParserRisliveError::IrregularRisLiveFormat;
+        assert_eq!(err.to_string(), "irregular ris live format");
+
+        let err = ParserRisliveError::ElemIncorrectPrefix("test".to_string());
+        assert_eq!(err.to_string(), "incorrect prefix string: test");
+
+        let err = ParserRisliveError::ElemUnknownOriginType("test".to_string());
+        assert_eq!(err.to_string(), "unknown origin type: test");
+
+        let err = ParserRisliveError::ElemIncorrectAggregator("test".to_string());
+        assert_eq!(err.to_string(), "incorrect aggregator string: test");
+
+        let err = ParserRisliveError::ElemIncorrectIp("test".to_string());
+        assert_eq!(err.to_string(), "incorrect IP string: test");
+
+        let err = ParserRisliveError::ElemEndOfRibPrefix;
+        assert_eq!(err.to_string(), "found 'eor' (End of RIB) prefix");
+    }
+    #[test]
+    fn test_ris_live_error_debug() {
+        let err = ParserRisliveError::IncorrectJson("test".to_string());
+        assert_eq!(format!("{:?}", err), "IncorrectJson(\"test\")");
+
+        let err = ParserRisliveError::IncorrectRawBytes;
+        assert_eq!(format!("{:?}", err), "IncorrectRawBytes");
+
+        let err = ParserRisliveError::UnsupportedMessage;
+        assert_eq!(format!("{:?}", err), "UnsupportedMessage");
+
+        let err = ParserRisliveError::IrregularRisLiveFormat;
+        assert_eq!(format!("{:?}", err), "IrregularRisLiveFormat");
+
+        let err = ParserRisliveError::ElemIncorrectPrefix("test".to_string());
+        assert_eq!(format!("{:?}", err), "ElemIncorrectPrefix(\"test\")");
+
+        let err = ParserRisliveError::ElemUnknownOriginType("test".to_string());
+        assert_eq!(format!("{:?}", err), "ElemUnknownOriginType(\"test\")");
+
+        let err = ParserRisliveError::ElemIncorrectAggregator("test".to_string());
+        assert_eq!(format!("{:?}", err), "ElemIncorrectAggregator(\"test\")");
+
+        let err = ParserRisliveError::ElemIncorrectIp("test".to_string());
+        assert_eq!(format!("{:?}", err), "ElemIncorrectIp(\"test\")");
+
+        let err = ParserRisliveError::ElemEndOfRibPrefix;
+        assert_eq!(format!("{:?}", err), "ElemEndOfRibPrefix");
+    }
+}
--- src/parser/rislive/messages/ris_rrc_list.rs
+++ src/parser/rislive/messages/ris_rrc_list.rs
@@ -1,4 +1,26 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct RisRrcList {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ris_rrc_list_serde() {
+        let ris_rrc_list = RisRrcList {};
+
+        // serialize RisRrcList
+        let serialized = serde_json::to_string(&ris_rrc_list).expect("Failed to serialize");
+
+        // the serialized RisRrcList would be '{}'
+        assert_eq!(serialized, "{}");
+
+        // deserialize RisRrcList
+        let deserialized: RisRrcList =
+            serde_json::from_str(&serialized).expect("Failed to deserialize");
+
+        assert_eq!(ris_rrc_list, deserialized);
+    }
+}
