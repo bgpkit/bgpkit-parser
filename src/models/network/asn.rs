c9e3d699d5d0cdb87d9cf3204bd0fee9cd0ab30d
Mingwei Zhang <mingwei@mwzhang.com>
2025-08-16 15:22:59 -0700

    fix clippy warning

--- src/models/network/asn.rs
+++ src/models/network/asn.rs
@@ -52,7 +52,7 @@ impl PartialEq for Asn {
 
 impl PartialOrd for Asn {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(&other))
+        Some(self.cmp(other))
     }
 }
 
