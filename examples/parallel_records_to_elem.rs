d72ca510fd05f6348f5b50471b29c0928ead23e5
Ties de Kock <ties@tiesdekock.nl>
2026-02-08 19:29:16 +0100

    Update example

--- examples/parallel_records_to_elem.rs
+++ examples/parallel_records_to_elem.rs
@@ -1,12 +1,12 @@
-use bgpkit_parser::Elementor;
 use bgpkit_parser::models::*;
 use bgpkit_parser::BgpkitParser;
+use bgpkit_parser::Elementor;
 use std::sync::Mutex;
 
 const CHUNK_SIZE: usize = 16_384;
 
 /// an very simple example that reads a remote BGP data file and print out the message count.
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://data.ris.ripe.net/rrc00/2026.02/bview.20260208.0800.gz";
 
     // Iterate over the file in order
@@ -23,7 +23,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let t0 = std::time::Instant::now();
     let cnt = parser.into_raw_record_iter().count();
     println!("Total number of records: {cnt}");
-    println!("Time elapsed: {:?}s (to chunk into raw records)", t0.elapsed());
+    println!(
+        "Time elapsed: {:?}s (to chunk into raw records)",
+        t0.elapsed()
+    );
 
     // Iterate over the BGPElem's after parsing with Elementor, using the default pattern where we mutate the elementor.
     let parser = BgpkitParser::new_cached(url, "/tmp").unwrap();
@@ -35,36 +38,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         if let Ok(record) = raw_record.parse() {
             cnt += elementor.record_to_elems(record).iter().count();
         }
-        cnt += 1;
     }
     println!("Total number of routes: {cnt}");
-    println!("Time elapsed: {:?}s (parse to RawRecord, turn into BgpElem using Elementor)", t0.elapsed());
+    println!(
+        "Time elapsed: {:?}s (parse to RawRecord, turn into BgpElem using Elementor)",
+        t0.elapsed()
+    );
 
-
-
-    // Iterate with a shared Elementor
-    // We then take chunks of records (taking the lock of the Mutex on the parser) and parse those in parallel.
-    //
-    // This is not the most beautiful pattern, but it works.
+    // Producer-consumer parallel iteration: a dedicated producer thread reads
+    // and decompresses chunks, sending them through a channel to worker threads
+    // that parse and convert records in parallel.
     let parser = BgpkitParser::new_cached(url, "/tmp").unwrap();
 
     let mut record_iter = parser.into_raw_record_iter().peekable();
-    let mut elementor = Elementor::new();
 
     let t0 = std::time::Instant::now();
 
     // Peek to see if the first element is a peer index table. If so, consume it and set it.
-    if let Some(potential_peer_index) = record_iter.peek().cloned() {
-        if let Ok(pit) = potential_peer_index.parse() {
-            if matches!(pit.message, MrtMessage::TableDumpV2Message(TableDumpV2Message::PeerIndexTable(_))) {
-                elementor.set_peer_table(pit)?;
-                record_iter.next(); // Consume peeked/cloned item on iterator.
-            }
+    let elementor = match record_iter.peek().cloned().and_then(|r| r.parse().ok()) {
+        Some(MrtRecord {
+            message: MrtMessage::TableDumpV2Message(TableDumpV2Message::PeerIndexTable(pit)),
+            ..
+        }) => {
+            record_iter.next();
+            Elementor::with_peer_table(pit)
         }
-    }
+        _ => Elementor::new(),
+    };
 
-    let iter = Mutex::new(record_iter);
     let num_threads = std::thread::available_parallelism()?.get();
+    let iter = Mutex::new(record_iter);
 
     let total: u64 = std::thread::scope(|s| {
         let elementor = &elementor;
@@ -76,7 +79,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                         // lock the iterator and take a chunk of records.
                         let chunk: Vec<_> = {
                             let mut it = iter.lock().unwrap();
-                            (&mut *it).take(CHUNK_SIZE).collect()
+                            it.by_ref().take(CHUNK_SIZE).collect()
                         };
                         if chunk.is_empty() {
                             break;
@@ -98,6 +101,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     });
 
     println!("Total records: {total}");
-    println!("Time elapsed: {:?} (set peer index table on Elementor, iterate in parallel over chunks)", t0.elapsed());
+    println!(
+        "Time elapsed: {:?} (set peer index table on Elementor, iterate in parallel over chunks)",
+        t0.elapsed()
+    );
     Ok(())
 }
