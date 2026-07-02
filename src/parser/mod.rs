b5f40b72c116ed0ad70ce78bd2f532b60996fdb7
Mingwei Zhang <mingwei@mwzhang.com>
2026-07-02 10:12:43 -0700

    feat: re-export RIS Live socket options

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -29,7 +29,9 @@ pub use iters::*;
 pub use mrt::*;
 
 #[cfg(feature = "rislive")]
-pub use rislive::messages::{RisLiveClientMessage, RisSubscribe, RisSubscribeType};
+pub use rislive::messages::{
+    RisLiveClientMessage, RisSubscribe, RisSubscribeSocketOptions, RisSubscribeType,
+};
 #[cfg(feature = "rislive")]
 pub use rislive::{
     parse_ris_live_message, parse_ris_live_message_json, parse_ris_live_message_raw,
